c73439a40557d2d369d83691d3a5ec5b6cd9e6f8
Sergi Trujillo <sergi.trujillo@gmail.com>
2020-05-03 12:38:20 +0200

    Iris tasks

---
+++ iris/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=' Activity for Intensive data Processing Systems subject',
+    author='Sergi Trujillo',
+    license='MIT',
+)
